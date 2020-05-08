b7c39ebc5c16b04e88b61389eaad4edad86d197f
Nikita <shriramnikita@gmail.com>
2020-05-08 22:29:41 +0530

    Added: 50% stake would resign for the day if won or loss

--- gambler.sh
+++ gambler.sh
@@ -8,11 +8,26 @@ stake=100
 
 
 #uc2 check win or loss
-winLossResult=$((RANDOM%2))
 
-if [ $winLossResult -eq 1 ]
-then
-	echo "Win"
-else
-	echo "Loss"
-fi
+#uc3 calulate : if won or loat 50% stake resign for the day
+
+
+function Result()
+{
+LOW_LIMIT=$(( $stake / 2 ))
+HIGH_LIMIT=$(($stake / 2 + $stake))
+	
+	while [[ $LOW_LIMIT -lt $stake && $HIGH_LIMIT -gt $stake ]]
+	do
+		winLossResult=$((RANDOM%2))
+		if [ $winLossResult -eq 1 ]
+		then
+			stake=$(($stake+1))
+		else
+
+			stake=$(($stake-1))
+		fi
+	done
+	echo "Stake:- "$stake
+}
+Result
