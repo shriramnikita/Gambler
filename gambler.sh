5b5242101c64e8d88d733445384e9ef132828119
Nikita <shriramnikita@gmail.com>
2020-05-08 23:58:55 +0530

    Added: calculate lucky and unlucky day

--- gambler.sh
+++ gambler.sh
@@ -41,13 +41,40 @@ do
 	done
 }
 
-Result
+
 
 #us5 calculate monthly loss or win
+
+function dailyProfit()
+{
 for((j=1;j<=20;j++))
 do
-	echo "Day_$j ${gambler[Day_$j]}"
-done 
+   echo "Day_$j ${gambler[Day_$j]}"
+done
+}
 
+
+#Uc6 calculate lucky unlucky day
+
+function luckiestUnlukiest()
+{
+   gambler[Day_0]=0
+
+   for((j=1;j<=20;j++))
+   do
+      k=$(( $j - 1 ))
+      gambler[Day_$j]=$(( ${gambler[Day_$j]} + ${gambler[Day_$k]} ))
+      echo  "Day$j   "${gambler[Day_$j]}
+   done | sort -k2 -nr |awk 'NR==20{print "Unluckiest: " $0}AND NR==1{print "Luckiest: " $0}'
+}
+
+
+function Gambler()
+{
+Result
+dailyProfit
+luckiestUnlukiest
 echo "Total profit" $( printf "%s\n" ${gambler[@]} | awk '{sum+=$0}END{print sum}')
+}
 
+Gambler
